495864cc47a51afa3575ebedcb76675901fa3fde
Mikhail Dolgov <otetc82@yandex.ru>
2021-01-19 22:16:44 +0300

    initial commit

---
+++ src/main/java/com/mihey/ziptos3/Ziptos3Application.java
@@ -0,0 +1,13 @@
+package com.mihey.ziptos3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ziptos3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ziptos3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/mihey/ziptos3/controller/S3Controller.java
@@ -0,0 +1,12 @@
+package com.mihey.ziptos3.controller;
+
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/upload")
+public class S3Controller {
+
+
+}
